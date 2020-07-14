6bae6a2283af71e92fd0fba7449b1d0c5ce7f7e4
jonatas <jonatas101@hotmail.com>
2020-07-14 15:11:21 -0300

    check id

--- SalaryRaise.java
+++ SalaryRaise.java
@@ -19,8 +19,20 @@ public class SalaryRaise{
       sc.nextLine();
       System.out.print("Name: ");
       String name = sc.nextLine();
+      sc.nextLine();
       System.out.print("Salary: ");
       double salary = sc.nextDouble();
+      Employee employee = new Employee(id,name,salary);
+      List<Employee> checkId = list.stream().filter(x -> x.getId() == id).findfirst().orElse(null);
+      for (String x : checkId){
+        if(checkId != null){
+          list.add(employee);
+        }else{
+          System.out.print("Erro! id já existente");
+          
+        }
+      }
+      System.out.println();
   }
 
 
