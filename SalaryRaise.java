64e3edb9b33fc7e0d4136382a412c69e1dfc3377
jonatas <jonatas101@hotmail.com>
2020-07-14 16:04:13 -0300

    creating the function hasId to check if the id exists.

--- SalaryRaise.java
+++ SalaryRaise.java
@@ -11,29 +11,31 @@ public class SalaryRaise{
     System.out.print("How many employees will be registered? ");
     int n = sc.nextInt();
     System.out.println();
-
+// Gathering data to include new employees and checking if the id already exists.
     for(int i=0;i<n;i++){
       System.out.print("Employee #"+ i+1 +":");
       System.out.print("Id: ");
       int id = sc.nextInt();
+      while(hasId(id) != null){
+        System.out.print("Error! Try another id number: ")
+        id = sc.nextInt();
+      }
       sc.nextLine();
       System.out.print("Name: ");
       String name = sc.nextLine();
-      sc.nextLine();
       System.out.print("Salary: ");
       double salary = sc.nextDouble();
       Employee employee = new Employee(id,name,salary);
-      List<Employee> checkId = list.stream().filter(x -> x.getId() == id).findfirst().orElse(null);
-      for (String x : checkId){
-        if(checkId != null){
-          list.add(employee);
-        }else{
-          System.out.print("Erro! id já existente");
-          
-        }
+      list.add(Employee);
+
       }
       System.out.println();
+
+      public boolean hasId(int id){
+        return List<Employee> checkId = list.stream().filter(x -> x.getId() == id).findfirst().orElse(null);
+      }
   }
+
 
 
 
